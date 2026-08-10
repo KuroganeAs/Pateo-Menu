806c03ab904350b1875bd7c40718c071c49873a8
KuroganeAs <kevinmlggamer95@gmail.com>
2026-08-10 11:44:14 +0900

    Configure Facebook Page URL to pateo.timor profile page

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // Configurable settings for the café owner
 const CONFIG = {
   pdfPath: 'Menu/MENU%20PAINES%20CAFETARIA.pdf',
-  facebookPageUrl: 'https://www.facebook.com/PateoDili/', // Public Business Page URL
+  facebookPageUrl: 'https://www.facebook.com/pateo.timor', // Café FB Page
   fbLoadTimeout: 3500 // Time in ms before showing fallback if Facebook widget fails
 };
 
